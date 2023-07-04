5e1b1850c44484a5e87087fd6a1a69c58ab94a04
Matias Masciotta <mmasciotta@inconcertcc.com>
2023-07-04 11:38:07 -0300

    found with 50 contacts

--- script.js
+++ script.js
@@ -2,11 +2,62 @@
 
 const puppeteer = require('puppeteer-core');
 
+// Ids a automatizar
 const ids = [
-    38068,
-    38067,
-    38062,
-];
+    32204,
+    32203,
+    32199,
+    32196,
+    32195,
+    32185,
+    32180,
+    32179,
+    32177,
+    32175,
+    32160,
+    32155,
+    32154,
+    32113,
+    32107,
+    32100,
+    32097,
+    32095,
+    32084,
+    32082,
+    32073,
+    32071,
+    32061,
+    32053,
+    32044,
+    32023,
+    31986,
+    31972,
+    31964,
+    31957,
+    31922,
+    31909,
+    31889,
+    31850,
+    31826,
+    31789,
+    31780,
+    31773,
+    31766,
+    31733,
+    31724,
+    31705,
+    31700,
+    31669,
+    31667,
+    31660,
+    31657,
+    31654,
+    31623,
+    31594
+  ];
+  
+  
+  
 // url gestion cliente
 const url = 'https://grupomasmovil.inconcertcc.com/mas/contact/people/view/';
 // credenciales login
@@ -14,7 +65,21 @@ const user = 'Matias';
 const pass = 'Matias12/';
 // nombre de automatizacion
 const automationName = 'LeadBruto900_C-G-TELECABLE';
-const dateControl = '29/06/2023';
+
+// Obtener la fecha actual
+const currentDate = new Date();
+
+// Obtener el día, el mes y el año de la fecha actual
+const day = currentDate.getDate();
+const month = currentDate.getMonth() + 1; // Los meses en JavaScript se indexan desde 0, por lo tanto se suma 1
+const year = currentDate.getFullYear();
+
+// Formatear la fecha con ceros a la izquierda si es necesario
+const formattedDate = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
+
+const dateControl = formattedDate;
+
+console.log(dateControl);
 
 async function automate() {
     const browser = await puppeteer.launch({
